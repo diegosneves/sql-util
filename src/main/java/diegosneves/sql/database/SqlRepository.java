f2b6b20b29f409cb5a5d3be2702f5d9a75d757b1
Diego Neves <neves.diegoalex@outlook.com>
2023-08-25 22:24:18 -0300

    feat(sql util): :art: ajusta o metodo para deletar um objeto e insere um findby

--- src/main/java/diegosneves/sql/database/SqlRepository.java
+++ src/main/java/diegosneves/sql/database/SqlRepository.java
@@ -3,11 +3,14 @@ package diegosneves.sql.database;
 import diegosneves.sql.connection.HibernateConnectionSingleton;
 import diegosneves.sql.repository.Repository;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
 
 /**
  * Interface com implementacoes para utilizar o SQL como base de dados.<br>
+ *
  * @param <T> Entidade que será persistida no banco de dados
  * @param <K> Tipo de dado que será utilizado pelo banco de dados para representar o ID(MySQL usa Long como ID por padrao).
  */
@@ -49,10 +52,33 @@ public interface SqlRepository<T, K> extends Repository<T, K> {
     @Override
     default void delete(T data) {
         Session session = getConnection();
+        Transaction transaction = null;
+
         try {
+            transaction = session.beginTransaction();
             session.delete(data);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         } finally {
             closeConnection(session);
         }
     }
+
+    default T findBy(String atributo, Object valor) {
+        Session session = getConnection();
+        T obj;
+        try {
+            Query<T> query = session.createQuery("FROM " + this.getObjType().getSimpleName() + " p WHERE p." + atributo + " = :valor", this.getObjType());
+            query.setParameter("valor", valor);
+            obj = query.uniqueResult();
+        } finally {
+            SqlRepository.closeConnection(session);
+        }
+        return obj;
+    }
+
 }
